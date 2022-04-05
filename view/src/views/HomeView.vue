c5063670a2cb555764edcbd3700206b778ad1baf
rickkky <rickkky@foxmail.com>
2022-04-05 19:22:13 +0800

    view: basic layout

--- view/src/views/HomeView.vue
+++ view/src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <header class="flex justify-between items-center p-1 border-b">
+      <img
+        class="w-10 h-10"
+        src="https://vuejs.org/images/logo.png"
+        alt="logo"
+      />
+      <nav>
+        <RouterLink class="mr-2 last:mr-0 text-gray-800" to="/">
+          Home
+        </RouterLink>
+        <RouterLink class="mr-2 last:mr-0 text-gray-800" to="/edit">
+          Edit
+        </RouterLink>
+        <RouterLink class="mr-2 last:mr-0 text-gray-800" to="/about">
+          About
+        </RouterLink>
+      </nav>
+    </header>
+  </div>
 </template>
