cfaa5645ed52c667be0af32945c2a0d07debec2c
rickkky <rickkky@foxmail.com>
2022-04-26 21:03:42 +0800

    refactor: remove unused error handler

--- packages/server/src/note/note.controller.ts
+++ packages/server/src/note/note.controller.ts
@@ -15,11 +15,7 @@ export class NoteController {
   async create(@Body() dto: CreateNoteDto) {
     const name = dto.name;
     const id = dto.id || name;
-    try {
-      await this.noteService.create(id, name);
-    } catch (error) {
-      throw new InternalServerErrorException(error);
-    }
+    await this.noteService.create(id, name);
     return 'success';
   }
 }
